e91dfbd970c1b773cb64e8a6e082e9a2a2788ebc
sbsingh3 <sbsingh3@uno.edu>
2023-12-01 02:19:32 -0600

    Updates

---
+++ precache-manifest.d44ca93a482cf906796bcf6e2dce0a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a05913f2d7e81caa611",
+    "url": "/Text-Analyzer/static/css/main.3932f97a.chunk.css"
+  },
+  {
+    "revision": "1a05913f2d7e81caa611",
+    "url": "/Text-Analyzer/static/js/main.1a05913f.chunk.js"
+  },
+  {
+    "revision": "a2c5b36bbcff5e4bb234",
+    "url": "/Text-Analyzer/static/js/1.a2c5b36b.chunk.js"
+  },
+  {
+    "revision": "f8e7d89bbde0ee8450dd",
+    "url": "/Text-Analyzer/static/js/2.f8e7d89b.chunk.js"
+  },
+  {
+    "revision": "17c174349632d6ed675e",
+    "url": "/Text-Analyzer/static/js/runtime~main.17c17434.js"
+  },
+  {
+    "revision": "f2cfeb0ede4feb39a6e16b07724daef3",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
